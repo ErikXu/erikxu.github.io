32ec3cbb10b24b4592a2237e9dba757a8e69d8b0
Erik Xu <81636759@qq.com>
2018-02-17 16:50:25 +0800

    Implement blog demo.

---
+++ app/main.js
@@ -0,0 +1,31 @@
+﻿requirejs.config({
+    baseUrl: 'app/',
+    paths: {
+        'text': '../lib/require/text',
+        'durandal': '../lib/durandal/js',
+        'plugins': '../lib/durandal/js/plugins',
+        'plugins/http': 'shared/http',
+        'transitions': '../lib/durandal/js/transitions',
+        'knockout': '../lib/knockout/knockout-3.1.0',
+        'jquery': '../lib/jquery/jquery-2.1.1.min',
+        'bootstrap': '../lib/bootstrap/js/bootstrap.min',
+        'shared': 'shared'        
+    }
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'jquery', 'knockout'], function (system, app, viewLocator, dialog, $, ko, toastr) {
+
+    system.debug(true);
+
+    app.title = "编程玩家";
+
+    app.configurePlugins({
+        router: true,
+        dialog: true
+    });
+
+    app.start().then(function () {
+        viewLocator.useConvention();
+        app.setRoot('viewmodels/shell', 'entrance', 'applicationHost');
+    });
+});
---
+++ app/viewmodels/shell.js
@@ -0,0 +1,17 @@
+﻿define(['jquery', 'knockout', 'plugins/router', 'bootstrap'], function ($, ko, router) {
+     var routes = [
+       { route: '', title:'主页', moduleId: 'viewmodels/home', nav: true },
+       { route: 'skill', title:'技能', moduleId: 'viewmodels/skill', nav: true },
+       { route: 'bookmark', title:'网摘', moduleId: 'viewmodels/bookmark', nav: true }
+    ];
+    
+    var vm = {
+        router: router,
+        activate: function () {
+            router.map(routes).buildNavigationModel().mapUnknownRoutes('shared/viewmodels/not-found');
+            return router.activate();
+        }
+    };
+
+    return vm;
+});
